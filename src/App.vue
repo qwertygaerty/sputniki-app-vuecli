eb73fae39a2882a1470d595eafbfe0b5d55555bf
qwertygaerty <54385591+qwertygaerty@users.noreply.github.com>
2022-04-15 16:42:08 +0700

    Переделал на Vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,47 @@
 <template>
 
   <section class="gallery-link">
-    <router-link to="/gallery">Галерея</router-link>
+    <router-link to="/gallery" @click="hideGalleryLink()" v-if="hideLinks">Галерея</router-link>
+    <router-link to="/weather" @click="hideGalleryLink()" v-if="!hideLinks">Погода</router-link>
   </section>
 
   <router-view></router-view>
-
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
-  components: {
+  components: {},
+  data: function () {
+    return {
+      hideLinks: true,
+    }
+  },
 
+  methods: {
+    hideGalleryLink() {
+      this.hideLinks  = !this.hideLinks
+    }
   }
+
 }
 </script>
 
 <style>
 /*gallery-link*/
 
-.gallery-link  {
+.gallery-link {
   position: relative;
   overflow: hidden;
   width: 100%;
-  height: 2rem;
+  height: 3rem;
   display: flex;
   align-items: center;
   justify-content: center;
   text-decoration: #1d9ffc;
   cursor: pointer;
+
 }
 
 .gallery-link * {
